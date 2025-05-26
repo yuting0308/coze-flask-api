cd0203bb32a7f7606da30c4c65c5ea9836fc6452
yuting0308 <stephanie71206@gmail.com>
2025-05-26 13:53:27 +0800

    Add files via upload

--- API+coze.py
+++ API+coze.py
@@ -2,12 +2,22 @@ from flask import Flask, request, jsonify
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import os
+import json
 
 app = Flask(__name__)
 
 # Google Sheets 設定
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('yujichuqing-119.json', scope)
+
+# 從環境變數讀取金鑰
+json_str = os.environ.get('GOOGLE_CREDENTIALS')
+if not json_str:
+    raise ValueError("未找到 GOOGLE_CREDENTIALS 環境變數，請確認 Render 環境設定")
+
+creds_dict = json.loads(json_str)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
 client = gspread.authorize(creds)
 spreadsheet = client.open('同行者心知AI用戶資料庫')
 worksheet = spreadsheet.sheet1
